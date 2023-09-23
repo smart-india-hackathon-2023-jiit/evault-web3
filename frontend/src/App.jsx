d8b28ff6db8cd966b422ed5ac4f4265f12037993
lonewolf1503 <harshmathur1503@gmail.com>
2023-09-23 15:53:52 +0530

    Services page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import Mainpage from "./components/Mainpage";
+import About from "./components/About";
+import Services from "./components/Services";
 export default function App() {
   return (
     <div>
       <Mainpage/>
+      <About />
+      <Services />
     </div>
   );
 }
--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,44 +1,45 @@
-const Navbar = () => {
+const Services = () => {
   return (
-    <nav className="bg-white text-white p-2 fixed top-0 left-0 w-full font-display">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img
-              src="../../public/law.jpg"
-              alt="Your Logo"
-              className="h-12 w-12"
-            />
+    <div className="p-6 bg-[#EEEEEE] h-screen w-screen flex justify-center items-center">
+      <div className="w-50px">
+        <div className="pb-8">
+          <h1 className="text-6xl text-[#053B50] font-bold text-center mb-8">
+            SERVICES
+          </h1>
+        </div>
+        <div className="grid grid-cols-2 gap-4 mb-5 pb-5">
+          {/* Left Div */}
+          <div className="bg-[#279EFF] text-white rounded-lg p-4 max-w-lg mx-auto transition-transform transform hover:scale-105 mr-2">
+            <div className="h-[200px] w-[400px] mb-4 overflow-hidden">
+              <img
+                src="../../public/files.jpg"
+                alt="Service 1"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+            <h2 className="text-4xl font-semibold mb-4">Service 1</h2>
+            <p className="text-lg text-[#64CCC5] mb-6">
+              Description of Service 1.
+            </p>
           </div>
-          <div className="space-x-6">
-            <a
-              href="About"
-              className="hover:text-[#053B50] hover:border-b-2 hover:border-[#053B50] px-2 py-1 rounded text-lg text-[#176B87]"
-            >
-              About Us
-            </a>
-            <a
-              href="#services"
-              className="hover:text-[#053B50] hover:border-b-2 hover:border-[#053B50] px-2 py-1 rounded text-lg text-[#176B87]"
-            >
-              Services
-            </a>
-            <a
-              href="#contact"
-              className="hover:text-[#053B50] hover:border-b-2 hover:border-[#053B50] px-2 py-1 rounded text-lg text-[#176B87]"
-            >
-              Contact
-            </a>
-            <button
-              className="bg-[#176B87] hover:bg-white hover:text-[#176B87] border-none"
-            >
-              Login
-            </button>
+          {/* Right Div */}
+          <div className="bg-[#176B87] text-white rounded-lg p-4 max-w-lg mx-auto transition-transform transform hover:scale-105 ml-2">
+            <div className="h-[200px] w-[400px] mb-4 overflow-hidden">
+              <img
+                src="../../public/lawyer.jpg"
+                alt="Service 2"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+            <h2 className="text-4xl font-semibold mb-4">Service 2</h2>
+            <p className="text-lg text-[#64CCC5] mb-6">
+              Description of Service 2.
+            </p>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
-export default Navbar;
+export default Services;
