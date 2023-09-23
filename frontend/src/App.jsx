fac5581c7c656b875a04050357a72f5b73467316
lonewolf1503 <harshmathur1503@gmail.com>
2023-09-23 16:09:39 +0530

    app.jsx updated due to changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Mainpage from "./components/Mainpage";
 import About from "./components/About";
 import Services from "./components/Services";
+import Contact from "./components/Contact";
 export default function App() {
   return (
     <div>
       <Mainpage/>
       <About />
       <Services />
+      <Contact />
     </div>
   );
 }
